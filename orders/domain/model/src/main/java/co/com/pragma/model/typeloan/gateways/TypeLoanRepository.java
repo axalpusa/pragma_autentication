65b838c206e2ab6719d3112f0079c176d28190dd
axelpuertas <axalpusa1125@gmail.com>
2025-08-26 18:46:10 -0500

    ADD: commit

--- orders/domain/model/src/main/java/co/com/pragma/model/typeloan/gateways/TypeLoanRepository.java
+++ orders/domain/model/src/main/java/co/com/pragma/model/typeloan/gateways/TypeLoanRepository.java
@@ -1,10 +1,8 @@
 package co.com.pragma.model.typeloan.gateways;
 
-import co.com.pragma.model.order.Order;
-import co.com.pragma.model.typeloan.TypeLoan;
 import reactor.core.publisher.Mono;
 
 public interface TypeLoanRepository {
     Mono < Boolean > existByIdTypeLon(Long idTypeLoan);
-    Mono < Boolean > betwenMaximum(Long idTypeLoan);
+    //Mono < Boolean > betwenMaximum(Long idTypeLoan);
 }
